1a8733edf314e9fb11624c25b6e1dd9a8528ffb6
Harshit-Maurya <44harshitmaurya@gmail.com>
2024-11-05 08:17:54 +0530

    add domain in index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ main().then(()=> console.log("MongoDB connect succesfully")).catch(err => consol
 // middleware
 app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:5173/', 'https://bookstore-git-main-harshit-mauryas-projects.vercel.app/'],
+    origin: ['http://localhost:5173/', 'https://bookstore-git-main-harshit-mauryas-projects.vercel.app/', 'https://bookstore-5ejr6vxqp-harshit-mauryas-projects.vercel.app/'],
     credentials: true
 }))
 
